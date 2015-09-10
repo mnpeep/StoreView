77f9a9d4dba6c375c99b0417e06227222b372554
Max Napadiy <maximn1@msn.com>
2015-09-10 16:41:34 -0700

    Initial Commit

---
+++ StoreViewTests/StoreViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  StoreViewTests.swift
+//  StoreViewTests
+//
+//  Created by Max Napadiy on 9/10/15.
+//  Copyright © 2015 Maxed Out Programming, Inc. All rights reserved.
+//
+
+import XCTest
+@testable import StoreView
+
+class StoreViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
