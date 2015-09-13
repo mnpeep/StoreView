c6311c646e8ca7e1f258fd8205a1aea2bc42d601
Max Napadiy <maximn1@msn.com>
2015-09-13 01:25:51 -0700

    License Settings

---
+++ StoreView/AppSettings.swift
@@ -0,0 +1,15 @@
+//
+//  AppSettings.swift
+//  StoreView
+//
+//  Created by Max Napadiy on 9/12/15.
+//  Copyright © 2015 Maxed Out Programming, Inc. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class AppSettings: NSManagedObject {
+    
+    @NSManaged var licenseUsers: String?
+}
